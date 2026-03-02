bdd8564cf6ba32521eaf9a7522db968c3f1d0fdf
Rodrigo Ian <rodrigoian191104@gmail.com>
2026-03-02 16:14:43 -0400

    Regras de negocio e validação de senha

--- src/service/UsuarioService.java
+++ src/service/UsuarioService.java
@@ -11,6 +11,30 @@ public class UsuarioService {
 	}
 	
 	public void cadastrar(Usuario usuario) {
+		//verifica se os campos não estão vazios
+		if(usuario.getNome().isEmpty() || usuario.getEmail().isEmpty() || usuario.getSenha().isEmpty()) {
+			throw new RuntimeException("Erro: Campos vazios");
+		}
+		
+		validarSenha(usuario.getSenha());
+		
+
 		dao.insert(usuario);
 	}
-}
+
+	
+	
+	private void validarSenha(String senha) {
+		if (senha.length() < 6) {
+			throw new RuntimeException("Senha fraca, por favor que ela tenha mais de 6 digitos e pelo menos um caracter especial");
+		}
+		//se tiver pelo menos um caracter especial
+		boolean temEspecial = senha.matches(".*[^a-zA-Z0-9].*");
+		
+		if(!temEspecial) {
+			throw new RuntimeException("Senha deve ter pelo menos um caracter especial");
+		}
+		}
+		
+	}
+
